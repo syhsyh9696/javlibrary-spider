b4bd82be5286ea7e7f914ccc55970827c8bcd290
Yuanhao Sun <sunyuanhao123456@gmail.com>
2017-04-08 16:48:19 +0800

    New genres ./resources/all_genres.txt

--- test-script/works.rb
+++ test-script/works.rb
@@ -4,11 +4,15 @@ require 'rest-client'
 require 'nokogiri'
 require 'pp'
 
-baseurl = "http://www.jav11b.com/cn/?v=javlijqf2e"
+baseurl = "http://jav11b.com/cn/?v=javlio354y"
 
 response = RestClient.get baseurl
 doc = Nokogiri::HTML(response.body)
-details, genres = Array.new, Array.new
+
+
+title, details, genres = String.new, Array.new, Array.new
+
+title =  doc.search('div[@id="video_title"]/h3/a').children.text
 
 doc.search('//div[@id="video_info"]/div[@class="item"]/table/tr/td[@class="text"]').map do |row|    
     details << row.children.text
@@ -28,4 +32,6 @@ doc.search('//div[@id="video_genres"]/table/tr/td[@class="text"]/span[@class="ge
 end
 pp video_genres
 
-
+test = doc.search('//img[@id="video_jacket_img"]').each do |row|
+    pp row['src']
+end
--- works_merge.rb
+++ works_merge.rb
@@ -8,8 +8,8 @@ def traverse_dir(file_path, io)
             end
         end
     else
-        #puts "File:#{File.basename(file_path)}, Size:#{File.size(file_path)}"
-        p File.basename(file_path).class
+        # puts "File:#{File.basename(file_path)}, Size:#{File.size(file_path)}"
+        # p File.basename(file_path).class
         io << File.basename(file_path) << "\n" if File.size(file_path) != 0
     end
 end
@@ -17,3 +17,19 @@ end
 io = File.open("all_file_path.txt", "w")
 tmp = traverse_dir("/home/server-admin/laosiji-script/works", io)
 io.close
+
+io_all = File.open("all_identifer.txt", "w")
+File.open("all_file_path.txt", "r") do |io|
+    while line = io.gets
+        line.chomp!
+        id = String.new
+        File.open("./works/#{line}", "r") do |f|
+            while line = f.gets
+                identifer = line.chomp.split[0]
+                id << identifer << "\n"
+            end
+            io_all << id 
+        end
+    end
+end
+io_all.close
