dd1458b0f39b9e9e9b94bcf4e53265e32a74f502
Yuanhao Sun <sunyuanhao123456@gmail.com>
2017-04-08 12:49:46 +0800

    New test

--- test-script/works.rb
+++ test-script/works.rb
@@ -8,16 +8,24 @@ baseurl = "http://www.jav11b.com/cn/?v=javlijqf2e"
 
 response = RestClient.get baseurl
 doc = Nokogiri::HTML(response.body)
-details = Array.new
+details, genres = Array.new, Array.new
 
 doc.search('//div[@id="video_info"]/div[@class="item"]/table/tr/td[@class="text"]').map do |row|    
     details << row.children.text
 end
 
-p details.class
-pp details
+video_id = details[0]
+video_date = details[1]
+video_director = details[2]
+video_maker = details[3]
+video_label = details[4]
+video_cast = details[-1]
+video_genres = String.new
 
-details_temp = doc.search('//div[@id="video_genres"]/table/tr/td[@class="text"]/span[@class="genre"]/a').each do |row|
-    p row.children.text
+pp details 
+doc.search('//div[@id="video_genres"]/table/tr/td[@class="text"]/span[@class="genre"]/a').each do |row|
+    video_genres << row.children.text << " "
 end
+pp video_genres
+
 
