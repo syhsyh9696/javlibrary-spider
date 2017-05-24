b82395e5458bc7ef6a60095a7feda1f1354c2cbb
Yuanhao Sun <sunyuanhao123456@gmail.com>
2017-05-24 21:21:59 +0800

    Add some files

--- name.rb
+++ name.rb
@@ -2,37 +2,51 @@
 
 require 'rest-client'
 require 'nokogiri'
+require 'mysql2'
 
-def author_page_num(nokogiri_doc)
-    last_page = 1
-    nokogiri_doc.search('//div[@class="page_selector"]/a[@class="page last"]').each do |row|
-        last_page = row['href'].split("=")[-1].to_i
+module Javlibrary
+    def Javlibrary.author_page_num(nokogiri_doc)
+        last_page = 1
+        nokogiri_doc.search('//div[@class="page_selector"]/a[@class="page last"]').each do |row|
+            last_page = row['href'].split("=")[-1].to_i
+        end
+        last_page
     end
-    last_page
-end
 
-firsturl = "http://www.javlibrary.com/cn/star_list.php?prefix="
+    def get_all_actor
+        firsturl = "http://www.jav11b.com/cn/star_list.php?prefix="
 
-'A'.upto('Z') do |alphabet|
-    tempurl = firsturl + alphabet
-    response = RestClient.get tempurl
+        client = Mysql2::Client.new(:host => "127.0.0.1",
+                                    :username => "root",
+                                    :password => "XuHefeng",
+                                    :database => "javlibrary_new")
+        'A'.upto('Z') do |alphabet|
+            tempurl = firsturl + alphabet
+            response = RestClient.get tempurl
 
-    doc = Nokogiri::HTML(response.body)
-    last_page = author_page_num(doc)
+            doc = Nokogiri::HTML(response.body)
+            last_page = author_page_num(doc)
 
-    text = String.new
-    1.upto(last_page) do |page_num|
-        temp_page_url = tempurl + "&page=#{page_num.to_s}"
-        response_page = RestClient.get temp_page_url
-        doc_page = Nokogiri::HTML(response_page.body)
-        doc_page.search('//div[@class="starbox"]/div[@class="searchitem"]/a').each do |row|
-            p row
-            text << row['href'].split("=")[-1] << "\n"
+            1.upto(last_page) do |page_num|
+                temp_page_url = tempurl + "&page=#{page_num.to_s}"
+                response_page = RestClient.get temp_page_url
+                doc_page = Nokogiri::HTML(response_page.body)
+                doc_page.search('//div[@class="starbox"]/div[@class="searchitem"]/a').each do |row|
+                    # row.text Actor.name
+                    # row['href'].split("=")[-1] Actor.label
+                    name = row.text; label = row['href'].split("=")[-1]
+                    begin
+                        client.query("INSERT INTO actor (actor_name, actor_label, type)
+                            VALUES ('#{name}', '#{label}', '#{alphabet}')")
+                    rescue
+                        next
+                    end
+                end
+            end
         end
     end
 
-    io = File.open("#{alphabet}.txt", "w")
-    io << text
-    text = ""
-    io.close
+    module_function :get_all_actor
 end
+
+Javlibrary::get_all_actor
